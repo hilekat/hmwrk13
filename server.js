0c8ec6a4dd9d07ae820e4053e7dc140eff33cf5c
hilekat <68864060+hilekat@users.noreply.github.com>
2021-01-23 01:45:30 -0600

    changes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const exphbs = require("express-handlebars"); 
-
+const path = require("path")
 const app = express(); 
 
 let PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(express.static("public")); 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main"
+})); 
+//handlebars.registerPartials(__dirname + "/views/burgers");
 app.set("view engine", "handlebars");
 
 const routes = require("./controllers/burgersController.js");
